e552c39d23771d82de60f9b5118a9d3c11399471
dayan <dayan@domU-12-31-39-0C-C4-96.compute-1.internal>
2011-02-15 16:40:55 -0500

    FIXED PHP BUG: Segmentation fault caused by sprintf!

--- bin/GenerateData.php
+++ bin/GenerateData.php
@@ -11,15 +11,27 @@
  */
 
 ini_set('include_path', '.:'.realpath(dirname(__FILE__).'/../lib'));
+ini_set('memory_limit', '128M');
+ini_set('output_buffering', '16384');
+error_reporting(E_ALL | E_STRICT);
+
+$_SERVER['HTTP_HOST'] = 'cli';
 require_once('conf.php');
 
 $con = Preferences::getConnection();
 // get all finalized, non-personal regattas and go to town!
-$res = $con->query('select id from regatta where type <> "personal"');
+$res = $con->query('select id from regatta where type <> "personal" and id = 372');
+$lst = array();
 while ($obj = $res->fetch_object()) {
+  $lst[] = $obj->id;
+}
+$res->free();
+
+foreach ($lst as $id) {
   try {
-    $reg = new Regatta($obj->id);
-    UpdateRegatta::runSync($reg);
+    $reg = new Regatta($id);
+    UpdateRegatta::run($reg, UpdateRequest::ACTIVITY_ROTATION);
+    UpdateRegatta::run($reg, UpdateRequest::ACTIVITY_SCORE);
     printf("(%3d) Imported regatta %s\n", $reg->id(), $reg->get(Regatta::NAME));
   }
   catch (Exception $e) {
