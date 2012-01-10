c7f1d3480d361e0d547f9cc0290b22a685fb71ea
Dayan Paez <dayan@marti>
2012-01-09 22:56:22 -0500

    Deprecated Preferences::getMessages (lingering in MessagePane).

--- lib/users/MessagePane.php
+++ lib/users/MessagePane.php
@@ -22,7 +22,7 @@ class MessagePane extends AbstractUserPane {
   }
 
   protected function fillHTML(Array $args) {
-    $messages = Preferences::getMessages($this->USER);
+    $messages = DB::getMessages($this->USER);
 
     // ------------------------------------------------------------
     // No messages
@@ -99,7 +99,7 @@ class MessagePane extends AbstractUserPane {
     // Delete
     // ------------------------------------------------------------
     if (isset($args['delete'])) {
-      $messages = Preferences::getMessages($this->USER);
+      $messages = DB::getMessages($this->USER);
       $mes = Preferences::getObjectWithProperty($messages, "id", $args['delete']);
       if ($mes === null) {
 	Session::pa(new PA("Invalid message to delete.", PA::E));
@@ -114,7 +114,7 @@ class MessagePane extends AbstractUserPane {
     // Reply
     // ------------------------------------------------------------
     if (isset($args['reply'])) {
-      $messages = Preferences::getMessages($this->USER);
+      $messages = DB::getMessages($this->USER);
       $mes = Preferences::getObjectWithProperty($messages, "id", $args['reply']);
       if ($mes === null) {
 	Session::pa(new PA("Invalid message to reply.", PA::E));
