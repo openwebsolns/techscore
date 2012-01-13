58026e4105dec5eab67e9fa6e3a1cb2ed3b753b0
Dayan Paez <dayan@marti>
2012-01-13 14:01:19 -0500

    Move Score class from stand-alone file to DB.php.
    Note that Score is not a DBObject. It is merely a convenience class
    used to track score and explanation properties together. Note that
    this entire class is a good condidate for deprecation in favor of
    adding methods to the Finish class which handle the two objects
    attributes logically.

--- lib/regatta/DB.php
+++ lib/regatta/DB.php
@@ -1247,4 +1247,36 @@ class TeamPenalty extends DBObject {
 		   $this->comments);
   }
 }
+
+/**
+ * Encapsulates a score. Objects can only be created. Their
+ * attributes are not setable after that.
+ *
+ * @author Dayan Paez
+ * @version 2010-01-30
+ */
+class Score {
+
+  private $score;
+  private $explanation;
+
+  /**
+   * Create a score with the given parameters
+   *
+   * @param int $score the numerical score
+   * @param String $exp the explanation
+   */
+  public function __construct($score, $exp = "") {
+    $this->score = (int)$score;
+    $this->explanation = $exp;
+  }
+
+  /**
+   * Fetches the given value
+   *
+   */
+  public function __get($name) {
+    return $this->$name;
+  }
+}
 ?>
