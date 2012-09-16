b9ff0ab8d436df57f94fa12d44d4b11964d08cd9
Dayan Paez <devnull@localhost>
2012-09-16 16:29:15 -0400

    Special TSEditor will automatically hyperlink URLs in regatta summaries.

---
+++ lib/xml5/TSEditor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of TechScore
+ */
+
+require_once(dirname(__FILE__).'/DPEditor.php');
+
+/**
+ * DPEditor subclass for TS
+ *
+ * @author Dayan Paez
+ * @created 2012-09-16
+ */
+class TSEditor extends DPEditor {
+  protected function preParse($inp) {
+    $inp = preg_replace('@(https?://[^\s]+)@', '{a:$1}', $inp);
+    return $inp;
+  }
+}
+?>
