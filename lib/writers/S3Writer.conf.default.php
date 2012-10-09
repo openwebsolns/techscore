f20e2e3e8adea336c9a456bc2e84ca49688a54cf
Dayan Paez <devnull@localhost>
2012-10-09 16:20:42 -0400

    First release of S3Writer candidate (complete with local conf settings).
    Note that the current S3Writer does not handle directory removals.

---
+++ lib/writers/S3Writer.conf.default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Configuration parameters for S3Writer.
+ *
+ * Copy this file to S3Writer.conf.local.php and update
+ *
+ * @author Dayan Paez
+ * @created 2012-10-09
+ * @package tscore/writers
+ */
+
+S3Writer::$BUCKET = '';
+S3Writer::$ACCESS_KEY = '';
+S3Writer::$SECRET_KEY = '';
+
+// Default value is s3.amazonaws.com
+//S3Writer::$HOST_BASE = '';
+?>
---
+++ lib/writers/S3Writer.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * This file is part of TechScore
+ *
+ * @package tscore/writers
+ */
+
+require_once('AbstractWriter.php');
+
+/**
+ * Writes directly to an S3 bucket
+ *
+ * @author Dayan Paez
+ * @created 2012-10-09
+ */
+class S3Writer extends AbstractWriter {
+  public static $BUCKET = 'live.collegesailing.org';
+  public static $ACCESS_KEY = 'AKIAJUTLT5IE6R5UZXNA';
+  public static $SECRET_KEY = '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';
+  public static $HOST_BASE = 's3.amazonaws.com';
+
+  public function __construct() {
+    if (self::$BUCKET === null) {
+      require_once(dirname(__FILE__) . '/S3Writer.conf.local.php');
+      if (empty(self::$BUCKET) ||
+          empty(self::$ACCESS_KEY) ||
+          empty(self::$SECRET_KEY) ||
+          empty(self::$HOST_BASE))
+        throw new TSWriterException("Missing parameters for S3Writer.");
+    }
+  }
+
+  /**
+   * Helper method: prepare the S3 request
+   *
+   */
+  protected function prepRequest(&$fname) {
+    $ch = curl_init(sprintf('https://%s.%s%s', self::$BUCKET, self::$HOST_BASE, $fname));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'TS3 Bot');
+    return $ch;                                               
+  }
+
+  public function write($fname, &$contents) {
+    $type = 'text/plain';
+    $size = strlen($contents);
+    $date = date('D, d M Y H:i:s T');
+    $md5 = base64_encode(md5($contents, true));
+
+    $headers = array();
+    $headers[] = sprintf('Host: %s.%s', self::$BUCKET, self::$HOST_BASE);
+    $headers[] = sprintf('Content-Length: %s', $size);
+    $headers[] = sprintf('Content-MD5: %s', $md5);
+    $headers[] = sprintf('Date: %s', $date);
+
+    $string_to_sign = sprintf("PUT\n%s\n\n%s\n/%s%s",
+                              $md5,
+                              $date,
+                              self::$BUCKET, $fname);
+    $signature = base64_encode(hash_hmac('sha1', $string_to_sign, self::$SECRET_KEY, true));
+    $headers[] = sprintf('Authorization: AWS %s:%s', self::$ACCESS_KEY, $signature);
+                              
+    $ch = $this->prepRequest($fname);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    if (($fp = fopen('php://temp', 'w+')) === false)
+      throw new TSWriterException("Unable to create temporary file for $fname");
+    fwrite($fp, $contents);
+    fseek($fp, 0);
+
+    curl_setopt($ch, CURLOPT_PUT, true);
+    curl_setopt($ch, CURLOPT_INFILE, $fp);
+    curl_setopt($ch, CURLOPT_INFILESIZE, $size);
+    if (($output = curl_exec($ch)) === false) {
+      $mes = curl_error($ch);
+      curl_close($ch);
+      throw new TSWriterException($mes);
+    }
+
+    $data = curl_getinfo($ch);
+    if ($data['http_code'] != 200)
+      throw new TSWriterException(sprintf("HTTP error %s: %s", $data['http_code'], $output));
+    curl_close($ch);
+  }
+
+  /**
+   * Removes the given file, which must not be a directory
+   *
+   */
+  public function remove($fname) {
+    $date = date('D, d M Y H:i:s T');
+
+    $headers = array();
+    $headers[] = sprintf('Host: %s.%s', self::$BUCKET, self::$HOST_BASE);
+    $headers[] = sprintf('Date: %s', $date);
+
+    $string_to_sign = sprintf("DELETE\n\n\n%s\n/%s%s",
+                              $date,
+                              self::$BUCKET, $fname);
+    $signature = base64_encode(hash_hmac('sha1', $string_to_sign, self::$SECRET_KEY, true));
+    $headers[] = sprintf('Authorization: AWS %s:%s', self::$ACCESS_KEY, $signature);
+                              
+    $ch = $this->prepRequest($fname);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    if (($output = curl_exec($ch)) === false) {
+      $mes = curl_error($ch);
+      curl_close($ch);
+      throw new TSWriterException($mes);
+    }
+
+    $data = curl_getinfo($ch);
+    if ($data['http_code'] > 400)
+      throw new TSWriterException(sprintf("HTTP error %s: %s", $data['http_code'], $output));
+    curl_close($ch);
+  }
+}
+?>
