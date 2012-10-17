528ab60792dd00e4f5e915364a5abbb6ed7358f8
Dayan Paez <devnull@localhost>
2012-10-17 11:08:18 -0400

    Added Google Analytics code to public page.

--- lib/xml5/TPublicPage.php
+++ lib/xml5/TPublicPage.php
@@ -91,6 +91,19 @@ class TPublicPage extends XPage {
     foreach ($this->getCSS() as $css)
       $this->head->add(new LinkCSS($css));
 
+    // Add Google Analytics code
+    $this->head->add(new XScript('text/javascript', null,
+                                 "var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5316542-1']);
+  _gaq.push(['_setDomainName', 'collegesailing.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"));
+
     // Navigation
     $this->body->add(new XDiv(array('class'=>'nav'),
                               array(new XH3("Navigate"),
