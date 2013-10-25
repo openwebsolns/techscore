9ee31bfcbf79f79110071ec1b276f8209f50aa8b
dayan <dayan@collegesailing.org>
2013-10-25 08:56:11 -0400

    Add try-catch statement around etc/copy-burgees.php

--- etc/copy-burgees.php
+++ etc/copy-burgees.php
@@ -15,10 +15,14 @@ $user = DB::getAccount('paez@mit.edu');
 $file = '/tmp/burgee.png';
 $_FILES['logo_file'] = array('name'=>'', 'tmp_name'=>$file, 'error'=>0, 'size'=>10);
 foreach (DB::getAll(DB::$SCHOOL, new DBCond('burgee', null, DBCond::NE)) as $school) {
+  try {
   echo $school->id, " ";
   $P = new EditLogoPane($user, $school);
   file_put_contents($file, base64_decode($school->burgee->filedata));
   $P->process(array());
+  } catch (SoterException $e) {
+    echo $e->getMessage();
+  }
   echo "\n";
 }
 ?>
