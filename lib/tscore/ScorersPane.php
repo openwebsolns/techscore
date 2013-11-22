70c02ee3c951581f8dbf6cfa0efbc484d1bb7182
Dayan Paez <devnull@localhost>
2013-11-22 10:29:45 -0500

    Search for scorers to add, rather than wade through listing.
    This could be considered a security improvement. So there's that!

--- lib/tscore/ScorersPane.php
+++ lib/tscore/ScorersPane.php
@@ -24,7 +24,7 @@ class ScorersPane extends AbstractPane {
     $p->addHelp("node9.html#SECTION00522000000000000000");
 
     // Get scorers
-    $p->add($tab = new XQuickTable(array(), array("Name", "Affiliation", "")));
+    $p->add($tab = new XQuickTable(array('class'=>'full left'), array("Name", "Affiliation", "")));
     $scorers = array();
     foreach ($this->REGATTA->getScorers() as $s) {
       $scorers[$s->id] = $s;
@@ -49,61 +49,52 @@ class ScorersPane extends AbstractPane {
     }
 
     // Form to add scorers
+    $search = DB::$V->incString($args, 'search', 1, 101, null);
     $this->PAGE->addContent($p = new XPort("Add scorers"));
-    $p->addHelp("node9.html#SECTION00522100000000000000");
-    $p->add($s_form = $this->createForm());
-    // Conferences
-    //   -Get chosen_conference
-    $chosen_conf = (isset($args['conference'])) ? 
-      DB::getConference($args['conference']) : 
-      $this->USER->school->conference;
+    $p->add($s_form = $this->createForm(XForm::GET));
+    $s_form->add(new XP(array('class'=>'search'),
+                        array("Search by name: ",
+                              new XTextInput('search', $search, array('size'=>60)),
+                              new XSubmitInput('go', "Go"))));
 
-    if ($chosen_conf === null) {
-      Session::pa(new PA("Invalid conference chosen. Defaulting to your own.", PA::I));
-      $chosen_conf = $this->USER->school->conference;
-    }
-
-    $confs = array();
-    foreach (DB::getConferences() as $conf)
-      $confs[$conf->id] = $conf;
-
-    $s_form->add($fi = new FItem("Conference:", $sel = XSelect::fromArray('conference', $confs, $chosen_conf->id)));
-    $sel->set("onchange","submit('this')");
-
-    // Add accessible submit button
-    $fi->add(new XSubmitAccessible("update_conf", "Update"));
-
-    // Accounts form
-    $p->add($s_form = $this->createForm());
-    $s_form->set('id', 'list');
-
-    // Get accounts for this conference
-    $NPP = 15;
-    $accounts = $chosen_conf->getUsers();
-    $count = count($accounts);
-    $npp = ceil($count / $NPP);
-    $num = DB::$V->incInt($_GET, 'r', 1, $npp + 1, 1);
-    require_once('xml5/LinksDiv.php');
-    if ($count > 0) {
-      $s_form->add(new XP(array(), "Choose an account to add from the list below and click \"Add scorer\" below."));
-      $s_form->add($l = new LinksDiv($npp, $num, sprintf('/score/%s/scorers', $this->REGATTA->id), array(), 'r', '#list'));
-      $s_form->add($tab = new XQuickTable(array('class'=>'full'), array("", "First name", "Last name", "School")));
-      for ($i = $NPP * ($num - 1); $i < $NPP * $num && $i < $count; $i++) {
-        $user = $accounts[$i];
-        if (!isset($scorers[$user->id])) {
-          $id = 's-' . $user->id;
-          $tab->addRow(array(new XRadioInput('account', $user->id, array('id'=>$id)),
-                             new XTD(array('class'=>'left'), new XLabel($id, $user->first_name)),
-                             new XTD(array('class'=>'left'), new XLabel($id, $user->last_name)),
-                             new XTD(array('class'=>'left'), new XLabel($id, $user->school->name))));
+    if ($search !== null) {
+      if (strlen($search) < 5) {
+      $p->add(new XP(array('class'=>'warning'), "Search term is too short. Must be at least 5 characters long."));
+      }
+      else {
+        require_once('regatta/Account.php');
+        $accnts = DB::searchAccounts($search, null, Account::STAT_ACTIVE);
+        if (count($accnts) == 0) {
+          $p->add(new XP(array('class'=>'warning'), "No results match your request. Please try again."));
+        }
+        else {
+          // Make sure that there are some sailors left to add
+          $num = 0;
+          $tab = new XQuickTable(array('class'=>'full left'), array("", "First name", "Last name", "School"));
+          $i = 0;
+          foreach ($accnts as $user) {
+            if (!isset($scorers[$user->id])) {
+              $id = 's-' . $user->id;
+              $tab->addRow(array(new XRadioInput('account', $user->id, array('id'=>$id)),
+                                 new XLabel($id, $user->first_name),
+                                 new XLabel($id, $user->last_name),
+                                 new XLabel($id, $user->school->name)),
+                           array('class'=>'row'.($i++ % 2)));
+              $num++;
+            }
+          }
+          if ($num > 0) {
+            $p->add($f = $this->createForm());
+            $f->add(new XP(array(), "Choose the sailor to add from the list below and click the \"Add scorer\" button."));
+            $f->add($tab);
+            $f->add(new XSubmitP("add_scorer", "Add scorer"));
+          }
+          else {
+            $p->add(new XP(array('class'=>'warning'), "There are no accounts left to register. Please try a different one."));
+          }
         }
       }
-      $s_form->add($l);
-      $s_form->add(new XSubmitP("add_scorer", "Add scorer"));
     }
-    else
-      $s_form->add(new XMessage("There are no accounts left to register in this conference. Please try a different one."));
-
   }
 
   public function process(Array $args) {
