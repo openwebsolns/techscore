a21fa9d4a0f8bb60edc1925e006c673b62bc40a8
Dayan Paez <devnull@localhost>
2013-12-13 18:19:12 -0500

    Do not use fancy selects for sail colors.

--- res/www/inc/js/mselect.js
+++ res/www/inc/js/mselect.js
@@ -179,7 +179,8 @@ window.addEventListener('load', function(evt) {
     for (var i = 0; i < selects.length; i++) {
         if (selects[i].multiple)
             new OWSMultSelect(selects[i]);
-        else if (selects[i].options.length > 10)
+        else if (selects[i].options.length > 10 && !selects[i].classList.contains("color-chooser")) {
             new OWSComboboxSelect(selects[i]);
+        }
     }
 }, false);
