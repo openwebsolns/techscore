c5d6fd5dd23db8105b62ac6804bb2ad8a3107e73
Dayan Paez <devnull@localhost>
2014-01-21 10:19:26 -0500

    Change mobile.js to match the goodness of form.js.
    This is long overdue.

--- res/www/inc/js/mobile.js
+++ res/www/inc/js/mobile.js
@@ -9,10 +9,10 @@ function toggleMenu () {
     // Get menu class
     var m = document.getElementById("menudiv");
     if (m.className.indexOf("hidden") >= 0) {
-	m.className = "";
+	      m.className = "";
     }
     else {
-	m.className = "hidden";
+	      m.className = "hidden";
     }
 }
 
@@ -26,7 +26,7 @@ function toggleMenu () {
 function makeRange(list) {
     // Must be unique
     if (list.length == 0)
-	return "";
+	      return "";
     
     list = sort_unique(list);
 
@@ -34,20 +34,20 @@ function makeRange(list) {
     var last  = list[0];
     var range = last;
     for (var i = 1; i < list.length; i++) {
-	if ((Number)(list[i]) == (Number)(last) + 1) {
-	    mid_range = true;
-	}
-	else {
-	    mid_range = false;
-	    if (last != range.substring(range.length-last.length))
-		range += "-" + last;
+	      if ((Number)(list[i]) == (Number)(last) + 1) {
+	          mid_range = true;
+	      }
+	      else {
+	          mid_range = false;
+	          if (last != range.substring(range.length-last.length))
+		            range += "-" + last;
 
-	    range += "," + list[i];
-	}
-	last = list[i];
+	          range += "," + list[i];
+	      }
+	      last = list[i];
     }
     if ( mid_range )
-	range += "-" + last;
+	      range += "-" + last;
 
     return range;
 }
@@ -60,51 +60,65 @@ function makeRange(list) {
 function parseRange(str) {
 
     if ( str.toUpperCase() == "ALL" )
-	return allowed;
+	      return allowed;
 
     if ( str.length == 0 )
-	return new Array();
+	      return new Array();
 
     var list = new Array();
     var n = 0; // Index for list
     // Separate value at commas
     var sub   = str.split(",");
     for (var i = 0; i < sub.length; i++) {
-	var delims = sub[i].split("-");
+	      var delims = sub[i].split("-");
 
-	for (var j = (Number)(delims[0]); j <= (Number)(delims[delims.length-1]); j++) {
-	    list[n] = j;
-	    n++;
-	}
+	      for (var j = (Number)(delims[0]); j <= (Number)(delims[delims.length-1]); j++) {
+	          list[n] = j;
+	          n++;
+	      }
     }
 
     return list;
 }
 
-// Global jQuery and user interface scripts
-//
-// February 3, 2009
-$(document).ready(function(){
-	  // Automatically hide class "accessible"
-	  $(".accessible").css("display","none");
+(function(w,d,g) {
+    var f = function(e) {
+        var acc = d.querySelectorAll(".accessible");
+        for (var i = 0; i < acc.length; i++)
+            acc[i].style.display = "none";
 
-  	// Calendar
-    var inp = document.getElementById("datepicker");
-    if (inp !== null && inp.type == "text") {
-        var s = document.createElement("script");
-        s.type = "text/javascript";
-        s.src = "/inc/js/ui.datepicker.js";
-        s.onload = function(evt) {
-	          $('#datepicker').datepicker({firstDay: 1,
-		                                     gotoCurrent: true,
-		                                     currentText: 'Current'
-		                                    });
+        // Announcements
+        var ul = d.getElementById("announcements");
+        if (ul) {
+            var cf = function(li) {
+                return function(e) {
+                    li.parentNode.removeChild(li);
+                };
+            };
+            for (i = 0; i < ul.childNodes.length; i++) {
+                var li = ul.childNodes[i];
+                li.style.position = "relative";
+                var a = document.createElement("img");
+                a.src = "/inc/img/c.png";
+                a.setAttribute("alt", "X");
+                a.style.position = "absolute";
+                a.style.top = "30%";
+                a.style.right = "0";
+                a.style.cursor = "pointer";
+                a.onclick = cf(li);
+                li.appendChild(a);
+            }
+        }
+        toggleMenu();
+    };
+    if (w.addEventListener)
+        w.addEventListener('load', f, false);
+    else {
+        var old = w.onload;
+        w.onload = function(e) {
+            f(e);
+            if (old)
+                old(e);
         };
-        var p = document.getElementsByTagName("script")[0];
-        p.parentNode.insertBefore(s, p);
-
     }
-
-	  toggleMenu();
-});
-
+})(window,document,"script");
