aa63916bb0e387a50a3cd1ae2602408e7e83dd84
Dayan Paez <dayan@marti>
2014-05-10 12:15:33 -0400

    Edit and add roles.

--- lib/users/admin/RoleManagementPane.php
+++ lib/users/admin/RoleManagementPane.php
@@ -20,7 +20,50 @@ class RoleManagementPane extends AbstractAdminUserPane {
     $this->page_url = 'roles';
   }
 
+  protected function fillRoleForm(XForm $form, Role $role) {
+    $form->add(new FReqItem("Name:", new XTextInput('title', $role->title, array('min'=>5, 'max'=>256))));
+    $form->add(new FReqItem("Description:", new XTextArea('description', $role->description, array('placeholder'=>"Helpful descriptors help you stay organized."))));
+    $form->add(new FReqItem("Permissions:", $sel = new XSelectM('permissions[]', array('size'=>10))));
+
+    // Fill select
+    $existing = array();
+    foreach ($role->getPermissions() as $perm)
+      $existing[$perm->id] = $perm;
+
+    $groups = array();
+    foreach (DB::getAll(DB::$PERMISSION) as $perm) {
+      if (!isset($groups[$perm->category])) {
+	$sel->add($group = new FOptionGroup($perm->category));
+	$groups[$perm->category] = $group;
+      }
+      $groups[$perm->category]->add($opt = new FOption($perm->id, $perm, array('title'=>$perm->description)));
+      if (isset($existing[$perm->id]))
+	$opt->set('selected', 'selected');
+    }
+  }
+
   protected function fillHTML(Array $args) {
+    // ------------------------------------------------------------
+    // Add new one?
+    // ------------------------------------------------------------
+    if (isset($args['create-from'])) {
+      $role = DB::$V->incID($args, 'create-from', DB::$ROLE);
+      if ($role === null)
+	$role = new Role();
+      else {
+	$role->title .= " (Copy)";
+      }
+
+      $this->PAGE->addContent($p = new XPort("Add role"));
+      $p->add($form = $this->createForm());
+      $this->fillRoleForm($form, $role);
+      $form->add($xp = new XSubmitP('add', "Add Role"));
+      $xp->add(" ");
+      $xp->add(new XA('/roles', "Cancel"));
+
+      return;
+    }
+
     // ------------------------------------------------------------
     // Edit existing?
     // ------------------------------------------------------------
@@ -30,25 +73,13 @@ class RoleManagementPane extends AbstractAdminUserPane {
         $this->PAGE->addContent($p = new XPort("Edit role"));
         $p->add($form = $this->createForm());
 
-        $form->add(new FReqItem("Name:", new XTextInput('title', $role->title, array('max'=>256))));
-        $form->add(new FItem("Description:", new XTextArea('description', $role->description, array('placeholder'=>"Helpful descriptors help you stay organized."))));
-        $form->add(new FReqItem("Permissions:", $sel = new XSelectM('permissions[]', array('size'=>10))));
+	$users = $role->getAccounts();
+	if (count($users) > 0) {
+	  $form->add(new XP(array(),
+			    array(new XStrong("Note:"), " these permissions will instantly apply to all ", count($users), " users that are associated with it.")));
+	}
 
-        // Fill select
-        $existing = array();
-        foreach ($role->getPermissions() as $perm)
-          $existing[$perm->id] = $perm;
-
-        $groups = array();
-        foreach (DB::getAll(DB::$PERMISSION) as $perm) {
-          if (!isset($groups[$perm->category])) {
-            $sel->add($group = new FOptionGroup($perm->category));
-            $groups[$perm->category] = $group;
-          }
-          $groups[$perm->category]->add($opt = new FOption($perm->id, $perm));
-          if (isset($existing[$perm->id]))
-            $opt->set('selected', 'selected');
-        }
+	$this->fillRoleForm($form, $role);
 
         $form->add($xp = new XSubmitP('edit', "Save Changes"));
         $xp->add(new XHiddenInput('role', $role->id));
@@ -61,10 +92,28 @@ class RoleManagementPane extends AbstractAdminUserPane {
       }
     }
 
+    $roles = DB::getAll(DB::$ROLE);
+
+    // ------------------------------------------------------------
+    // Add a role
+    // ------------------------------------------------------------
+    $this->PAGE->addContent($p = new XPort("Add a Role"));
+    $p->add($form = $this->createForm(XForm::GET));
+    if (count($roles) > 0) {
+      $form->add(new XP(array(), "Create a new role from scratch or by choosing an existing role as template."));
+      $form->add(new FItem("Copy existing:", $sel = new XSelect('create-from')));
+      $sel->add(new FOption('new', "[Start from scratch]"));
+      foreach ($roles as $role)
+	$sel->add(new FOption($role->id, $role, array('title'=>$role->description)));
+    }
+    else {
+      $form->add(new XHiddenInput('create-from', 'new'));
+    }
+    $form->add(new XSubmitP('go', "Create →"));
+
     // ------------------------------------------------------------
     // List current roles
     // ------------------------------------------------------------
-    $roles = DB::getAll(DB::$ROLE);
     if (count($roles) > 0) {
       $this->PAGE->addContent($p = new XPort("Existing roles"));
       $p->add(new XP(array(), "Below is a list of existing roles and their associated permissions. You may only delete roles which are not assigned. Click on the role name to edit that role."));
@@ -72,15 +121,19 @@ class RoleManagementPane extends AbstractAdminUserPane {
       $p->add($form = $this->createForm());
       $form->add($tab = new XQuickTable(array('class'=>'roles-table full'),
                                         array("Role", "Description", "Permissions", "# of Users", "")));
+      $can_delete = false;
       foreach ($roles as $i => $role) {
         $perms = new XUl(array('class'=>'role-permissions'));
-        foreach ($role->getPermissions() as $perm)
-          $perms->add(new XLi($perm));
+        foreach ($role->getPermissions() as $perm) {
+          $perms->add(new XLi($perm, array('title' => $perm->description)));
+	}
 
         $count = count($role->getAccounts());
         $del = "";
-        if ($count == 0)
-          $del = new FCheckbox('delete[]', $role->id, "");
+        if ($count == 0) {
+	  $can_delete = true;
+          $del = new FCheckbox('roles[]', $role->id, "");
+	}
 
         $tab->addRow(array(
                        new XA(sprintf('/roles?id=%s', $role->id), $role),
@@ -92,12 +145,44 @@ class RoleManagementPane extends AbstractAdminUserPane {
                      array('class' => 'row' . ($i % 2)));
       }
 
-      $form->add(new XSubmitP('edit', "Delete checked", array(), true));
+      if ($can_delete)
+	$form->add(new XSubmitP('delete', "Delete checked", array(), true));
     }
   }
 
   public function process(Array $args) {
-    throw new SoterException("Nothing to process here.");
+    if (isset($args['edit']) || isset($args['add'])) {
+      $role = new Role();
+      $mes = "added";
+      if (isset($args['edit'])) {
+	$role = DB::$V->reqID($args, 'role', DB::$ROLE, "Invalid role provided.");
+	$mes = "updated";
+      }
+      $role->title = DB::$V->reqString($args, 'title', 5, 256, "Invalid title provided for role.");
+      $role->description = DB::$V->reqString($args, 'description', 5, 1000, "Invalid description. Did you include enough?");
+
+      // Duplicate title?
+      foreach (DB::getAll(DB::$ROLE) as $other) {
+	if ($other->id != $role->id && $other->title == $role->title)
+	  throw new SoterException("Duplicate role title provided.");
+      }
+
+      $perms = array();
+      foreach (DB::$V->reqList($args, 'permissions', null, "No list of permissions provided.") as $id) {
+	$perm = DB::get(DB::$PERMISSION, $id);
+	if ($perm === null)
+	  throw new SoterException("Invalid permission provided: " . $id);
+	$perms[] = $perm;
+      }
+
+      if (count($perms) == 0)
+	throw new SoterException("Roles must have at least one permission set.");
+
+      DB::set($role);
+      $role->setPermissions($perms);
+      Session::pa(new PA(sprintf("Role \"%s\" %s.", $role, $mes)));
+      $this->redirect('roles');
+    }
   }
 }
 ?>
