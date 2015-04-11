a3f4d26126584313c00462d8f985dffe5ff8645d
Dayan Paez <devnull@localhost>
2015-04-11 10:47:22 -0700

    Add non-complete unit test for FleetRpValidator.

---
+++ tst/unit/FleetRpValidatorTest.php
@@ -0,0 +1,57 @@
+<?php
+use \tscore\utils\FleetRpValidator;
+
+require_once('AbstractUnitTester.php');
+
+/**
+ * Tests the FleetRpValidator functionality.
+ *
+ * @author Dayan Paez
+ * @version 2015-04-11
+ */
+class FleetRpValidatorTest extends AbstractUnitTester {
+
+  private $validator;
+
+  protected function setUp() {
+    $this->validator = new FleetRpValidator(new TestRegatta());
+  }
+
+  /**
+   * @expectedException SoterException
+   * @expectedExceptionMessage Missing RP data.
+   */
+  public function testMungedInput() {
+    $input = array(
+      'rp' => 'foo'
+    );
+    $this->validator->validate($input, new Team());
+  }
+
+  /**
+   * @expectedException SoterException
+   * @expectedExceptionMessage Missing RP data for A division.
+   */
+  public function testMungedInputDivision() {
+    $input = array(
+      'rp' => array(
+        'DivisionA' => array(), // invalid
+        'B' => array(),
+      ),
+    );
+    $this->validator->validate($input, new Team());
+  }
+}
+
+/**
+ * Regatta used in testing.
+ *
+ * @author Dayan Paez
+ * @version thedate
+ */
+class TestRegatta extends FullRegatta {
+    public function isFleetRacing() { return true; }
+    public function getDivisions() {
+      return array(Division::A(), Division::B());
+    }
+}
