0d746336a76731c3422880ea4a1d0fd84634488e
Dayan Paez <dayan@openweb-solutions.net>
2024-03-18 06:43:32 -0400

    v3.6.1: release

--- lib/conf.php
+++ lib/conf.php
@@ -13,7 +13,7 @@ class Conf {
   /**
    * Techscore version.
    */
-  const APP_VERSION = '3.6';
+  const APP_VERSION = '3.6.1';
 
   /**
    * @var String the hostname (sans protocol) for the scoring
