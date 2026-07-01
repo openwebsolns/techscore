cc6d6beb4a1d819d0e7e33883756e9de5bb522f4
Dayan Paez <dayan@openweb-solutions.net>
2026-07-01 09:50:04 -0400

    feat(cdk): migrate all other crontabs

--- aws-cdk/lib/constructs/Crontab.ts
+++ aws-cdk/lib/constructs/Crontab.ts
@@ -40,6 +40,105 @@ export class Crontab extends Construct {
       minute: "24",
       hour: "3",
     });
+
+    // Update the 404 pages, once a month works
+    this.newJob("Update404", ["-f", "general"], {
+      minute: "26",
+      hour: "4",
+      day: "13",
+    });
+
+    // Remove private regattas once a month
+    this.newJob("RemovePrivate", ["-vvv"], {
+      minute: "26",
+      hour: "5",
+      day: "28",
+    });
+
+    // Remove stale burgees twice a month
+    this.newJob("UpdateBurgee", ["-cvvv"], {
+      minute: "17",
+      hour: "2",
+      day: "13,27",
+    });
+
+    // Process mail messages
+    this.newJob("ProcessOutbox", ["-vvv"], {
+      minute: "37",
+    });
+
+    // Process mail bounces
+    this.newJob("ProcessBouncedEmails", ["-vvv"], {
+      minute: "55",
+      hour: "8",
+    });
+
+    // Update Twitter settings
+    this.newJob("TwitterCheck", ["-vvv"], {
+      minute: "39",
+      hour: "1",
+    });
+
+    // Send summary tweets on Thursdays, at 6PM
+    this.newJob("TweetSummary", ["-vvv", "coming_soon"], {
+      minute: "0",
+      hour: "18",
+      weekDay: "4",
+    });
+
+    // Remove stale sessions from database
+    this.newJob("CleanupSessions", ["-vvv"], {
+      minute: "43",
+    });
+    this.newJob("CleanupWebsessionLogs", ["-vvv"], {
+      minute: "47",
+    });
+    this.newJob("CleanupMetrics", ["-vvv"], {
+      minute: "49",
+      hour: "2",
+      day: "4",
+    });
+
+    // Remove completed update queues from database
+    this.newJob("CleanupCompletedUpdates", ["-vvv"], {
+      minute: "31",
+      hour: "3",
+      day: "3",
+    });
+
+    // Email users about pending regattas
+    this.newJob("RemindPending", ["-vvv"], {
+      minute: "0",
+      hour: "18",
+      weekDay: "2",
+    });
+    this.newJob("RemindMissingRP", ["-vvv"], {
+      minute: "0",
+      hour: "22",
+    });
+    this.newJob("RemindUpcoming", ["-vvv"], {
+      minute: "0",
+      hour: "18",
+      weekDay: "4",
+    });
+    this.newJob("AutoFinalize", ["-vvv"], {
+      minute: "0",
+      hour: "22",
+    });
+
+    // Update sailor information: do this every day around 4AM
+    this.newJob("SyncDB", ["--log", "-vvv", "schools", "sailors"], {
+      minute: "18",
+      hour: "4",
+    });
+    this.newJob("MergeUnregisteredSailors", ["-vvv"], {
+      minute: "27",
+      hour: "4",
+    });
+    this.newJob("RolloverEligibleSailors", ["auto", "-vvv"], {
+      minute: "33",
+      hour: "5",
+    });
   }
 
   private newJob(
